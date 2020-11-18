9f211932e31f4becac0d2754a46ff11f0a350347
SwordHeart <zxjlm233@163.com>
2020-11-18 11:33:12 +0800

    添加注释

--- mori.py
+++ mori.py
@@ -1,3 +1,12 @@
+"""
+@project : Mori
+@author  : zhang_xinjian
+@mail   : zxjlm233@163.com
+@ide    : PyCharm
+@time   : 2020-11-17 19:23:27
+@description: None
+"""
+
 import requests
 import json
 import re
@@ -7,24 +16,36 @@ from rich.console import Console
 from rich.traceback import Traceback
 import platform
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
-from printer import SimpleResult, ResultPrinter
-import xlwt
+from printer import ResultPrinter
 from reporter import Reporter
 from proxy import Proxy
 
-__version__ = 'v0.5'
+__version__ = 'v0.6'
 module_name = "Mori Kokoro"
 
 
 class MoriFuturesSession(FuturesSession):
-    def request(self, method, url, hooks={}, *args, **kwargs):
+    """
+    自定义的FuturesSession类，主要是重写了request函数，使其具有统计链接请求时间的功能
+    """
+
+    def request(self, method, url, hooks=None, *args, **kwargs):
+        """
+        重写的request函数，添加hooks
+        """
+        if hooks is None:
+            hooks = {}
         start = time.monotonic()
 
-        def response_time(resp, *args, **kwargs):
+        def response_time(resp, *args_sub, **kwargs_sub):
+            """
+            计算准确的请求时间
+            """
+            _, _ = args_sub, kwargs_sub
             resp.elapsed = time.monotonic() - start
             return
 
-        hooks['response'] = [response_time]
+        hooks['response'] = response_time
 
         return super(MoriFuturesSession, self).request(method,
                                                        url,
@@ -33,14 +54,28 @@ class MoriFuturesSession(FuturesSession):
 
 
 def regex_checker(regex, resp_json, exception=None):
-    res = regex_checker_recur(regex.split('->'), resp_json)
+    """
+    调用检查regex的方法，并且进行后续的包装处理
+    """
+    try:
+        error = None
+        res = regex_checker_recur(regex.split('->'), resp_json)
+    except TypeError as ty_e:
+        error = f'can`t match , {ty_e}'
+        res = None
+    except Exception as _e:
+        res = None
+        error = _e
     if exception:
-        return 'OK' if res == exception else 'Damage'
+        return 'OK' if res == exception else error
     else:
-        return 'OK' if len(res) > 0 else 'Damage'
+        return 'OK' if res else error
 
 
 def regex_checker_recur(regexs, resp_json):
+    """
+    递归遍历regex
+    """
     is_index = re.search(r'\$(\d+)\$', regexs[0])
     if len(regexs) == 1:
         return resp_json[regexs[0]]
@@ -51,50 +86,107 @@ def regex_checker_recur(regexs, resp_json):
 
 
 def data_render(apis):
-    def coloring(api):
-        for key, value in api.items():
+    """
+    渲染data数据，将{{*}}包裹的数据具体化
+    """
+
+    def coloring(api_sub):
+        """
+        递归渲染
+        """
+        for key, value in api_sub.items():
             if isinstance(value, list):
                 for val in value:
                     coloring(val)
             if isinstance(value, dict):
                 coloring(value)
             if value == "{{time}}":
-                api[key] = str(int(time.time() * 1000))
+                api_sub[key] = str(int(time.time() * 1000))
 
     for idx, api in enumerate(apis):
         if "data" in api.keys():
             coloring(apis[idx]['data'])
 
 
-def get_response(request_future, social_network):
+def get_response(request_future, site_data):
+    """
+    对response进行初步处理
+    """
     response = None
+    check_result = 'Damage'
+    check_results = []
+    traceback = None
 
-    error_context = "General Unknown Error"
-    expection_text = None
     try:
         response = request_future.result()
-        if response.status_code:
-            error_context = None
+        exception_text = getattr(response, 'exceptions', None)
+        error_context = getattr(response, 'error_context', None)
+
+        if response:
+            resp_text = response.text
+        else:
+            return
+
+        resp_json = {}
+
+        if site_data.get('decrypt') and resp_text:
+            try:
+                import importlib
+                package = importlib.import_module(
+                    'decrypt.' + site_data['decrypt'])
+                Decrypt = getattr(package, 'Decrypt')
+                resp_text = Decrypt().decrypt(resp_text)
+            except Exception as _e:
+                traceback = Traceback()
+                error_context = 'json decrypt error'
+                exception_text = _e
+
+        if resp_text:
+            try:
+                resp_json = json.loads(
+                    re.search('({.*})', resp_text.replace('\\', '')).group(1))
+            except Exception as _e:
+                traceback = Traceback()
+                error_context = 'response data not json format'
+                exception_text = _e
+            try:
+                check_results = {regex: regex_checker(
+                    regex, resp_json, site_data.get('exception'))
+                    for regex in site_data['regex']}
+
+                if list(check_results.values()) != ['OK'] * len(check_results):
+                    error_context = 'regex failed'
+                    check_result = 'Damage'
+                else:
+                    check_result = 'OK'
+
+            except Exception as _e:
+                traceback = Traceback()
+                error_context = 'json decrypt error'
+                exception_text = _e
     except requests.exceptions.HTTPError as errh:
         error_context = "HTTP Error"
-        expection_text = str(errh)
+        exception_text = str(errh)
     except requests.exceptions.ProxyError as errp:
         error_context = "Proxy Error"
-        expection_text = str(errp)
+        exception_text = str(errp)
     except requests.exceptions.ConnectionError as errc:
         error_context = "Error Connecting"
-        expection_text = str(errc)
+        exception_text = str(errc)
     except requests.exceptions.Timeout as errt:
         error_context = "Timeout Error"
-        expection_text = str(errt)
+        exception_text = str(errt)
     except requests.exceptions.RequestException as err:
         error_context = "Unknown Error"
-        expection_text = str(err)
+        exception_text = str(err)
 
-    return response, error_context, expection_text
+    return response, error_context, exception_text, check_results, check_result, traceback
 
 
-def mori(site_datas, result_printer, timeout):
+def mori(site_datas, result_printer, timeout) -> list:
+    """
+    主处理函数
+    """
     if len(site_datas) >= 20:
         max_workers = 20
     else:
@@ -103,13 +195,12 @@ def mori(site_datas, result_printer, timeout):
     session = MoriFuturesSession(
         max_workers=max_workers, session=requests.Session())
 
-    results_total = []
+    results_total, error_text, exception_text, check_result, check_results = [], '', '', 'Unknown', []
 
     for site_data in site_datas:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
         }
-        check_result = 'Damage'
         traceback, r, resp_text = None, None, ''
 
         if site_data.get('proxy'):
@@ -124,7 +215,7 @@ def mori(site_datas, result_printer, timeout):
                 try:
                     import importlib
                     package = importlib.import_module('antispider.' + site_data['antispider'])
-                    Antispider = package.Antispider
+                    Antispider = getattr(package, 'Antispider')
                     site_data['data'], headers = Antispider(
                         site_data['data'], headers).processor()
                 except Exception as _e:
@@ -138,7 +229,6 @@ def mori(site_datas, result_printer, timeout):
                             site_data['url'], json=site_data['data'], headers=headers, timeout=timeout, proxies=proxies,
                             allow_redirects=True)
                     else:
-                        # data = json.dumps(site_data['data'])
                         site_data["request_future"] = session.post(
                             site_data['url'], data=site_data['data'], headers=headers, timeout=timeout, proxies=proxies,
                             allow_redirects=True)
@@ -146,40 +236,12 @@ def mori(site_datas, result_printer, timeout):
                     site_data["request_future"] = session.get(
                         site_data['url'], headers=headers, timeout=timeout, proxies=proxies)
                 future = site_data["request_future"]
-                r, error_text, expection_text = get_response(request_future=future,
-                                                             social_network=site_data['name'])
+                r, error_text, exception_text, check_results, check_result, traceback = get_response(
+                    request_future=future,
+                    site_data=site_data)
 
                 if not error_text:
                     break
-
-            if r:
-                resp_text = r.text
-
-            if site_data.get('decrypt') and resp_text:
-                try:
-                    import importlib
-                    package = importlib.import_module(
-                        'decrypt.' + site_data['decrypt'])
-                    Decrypt = package.Decrypt
-                    resp_text = Decrypt().decrypt(resp_text)
-                except Exception as _e:
-                    raise Exception('json decrypt error')
-
-            if resp_text:
-                try:
-                    resp_json = json.loads(
-                        re.search('({.*})', resp_text.replace('\\', '')).group(1))
-                except Exception as _e:
-                    raise Exception('response data not json format')
-                try:
-                    for regex in site_data['regex']:
-                        check_result = regex_checker(
-                            regex, resp_json, site_data.get('exception'))
-                        if check_result != 'OK':
-                            error_text = 'regex failed'
-                            break
-                except Exception as _e:
-                    raise Exception('regex failed.')
 
             result = {
                 'name': site_data['name'],
@@ -189,9 +251,10 @@ def mori(site_datas, result_printer, timeout):
                 'status_code': r.status_code,
                 'time(s)': r.elapsed,
                 'error_text': error_text,
-                'expection_text': expection_text,
+                'expection_text': exception_text,
                 'check_result': check_result,
-                'traceback': traceback
+                'traceback': traceback,
+                'check_results': check_results
             }
 
             rel_result = result.copy()
@@ -207,7 +270,8 @@ def mori(site_datas, result_printer, timeout):
                 'time(s)': r and r.elapsed,
                 'error_text': error or 'site handler error',
                 'check_result': check_result,
-                'traceback': Traceback()
+                'traceback': Traceback(),
+                'check_results': check_results
             }
             rel_result = result.copy()
 
@@ -218,11 +282,14 @@ def mori(site_datas, result_printer, timeout):
 
 
 def timeout_check(value):
+    """
+    检查是否超时
+    """
     from argparse import ArgumentTypeError
 
     try:
         timeout = float(value)
-    except:
+    except Exception as _:
         raise ArgumentTypeError(f"Timeout '{value}' must be a number.")
     if timeout <= 0:
         raise ArgumentTypeError(
@@ -231,6 +298,10 @@ def timeout_check(value):
 
 
 def send_mail(receivers: list, file_content, html, subject, mail_host, mail_user, mail_pass, mail_port=0):
+    """
+    发送邮件
+    配置信息见README
+    """
     import smtplib
     from email.mime.text import MIMEText
     from email.mime.multipart import MIMEMultipart
@@ -268,8 +339,11 @@ def send_mail(receivers: list, file_content, html, subject, mail_host, mail_user
 
 
 def main():
+    """
+    入口
+    """
     version_string = f"%(prog)s {__version__}\n" + \
-                     f"{requests.__description__}:  {requests.__version__}\n" + \
+                     f"requests:  {requests.__version__}\n" + \
                      f"Python:  {platform.python_version()}"
 
     parser = ArgumentParser(formatter_class=RawDescriptionHelpFormatter,
@@ -321,13 +395,13 @@ def main():
 
     with open(file_path, 'r', encoding='utf-8') as f:
         apis = json.load(f)
+    console.print(f'[green] read file {file_path} success~')
     data_render(apis)
 
     if args.show_site_list:
 
         keys_to_show = ['name', 'url', 'data']
-        apis_to_show = list(map(lambda api: {key: value for key,
-                                                            value in api.items() if key in keys_to_show}, apis))
+        apis_to_show = list(map(lambda api: {key: value for key, value in api.items() if key in keys_to_show}, apis))
         console.print(apis_to_show)
 
     else:
@@ -345,11 +419,15 @@ def main():
 
         results = mori(apis, result_printer, timeout=args.timeout or 15)
 
+        for i, result in enumerate(results):
+            results[i]['check_results'] = '\n'.join(
+                [f'{key} : {value}' for key, value in result['check_results'].items()])
+
         if args.xls:
             console.print('[cyan]now generating report...')
 
             repo = Reporter(['name', 'url', 'status_code', 'time(s)',
-                             'check_result', 'error_text', 'resp_text'], results)
+                             'check_result', 'check_results', 'error_text', 'resp_text'], results)
             repo.processor()
 
             console.print('[green]mission completed')
@@ -357,12 +435,13 @@ def main():
         if args.email:
             try:
                 import config
-            except:
+            except Exception as _e:
                 console.print(
-                    'can`t get config.py file, please read README.md, search keyword [red]config.py')
+                    'can`t get config.py file, please read README.md, search keyword [red]config.py', _e)
+                return
             console.print('[cyan]now sending email...')
             repo = Reporter(['name', 'url', 'status_code', 'time(s)',
-                             'check_result', 'error_text', 'resp_text'], results)
+                             'check_result', 'check_results', 'error_text', 'resp_text'], results)
             fs = repo.processor(is_stream=True)
             html = repo.generate_table()
             try:
