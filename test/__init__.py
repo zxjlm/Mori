208ac7cd7d60685c947b419e8fc9352e1c63f55e
harumonia <zxjlm233@163.com>
2020-11-28 09:55:35 +0800

    add test file
    Signed-off-by: harumonia <zxjlm233@163.com>

---
+++ test/__init__.py
@@ -0,0 +1,10 @@
+"""
+@project : Mori
+@author  : zhang_xinjian
+@mail   : zxjlm233@163.com
+@ide    : PyCharm
+@time   : 2020-11-28 09:48:42
+@description: None
+"""
+
+
---
+++ test/test_main.py
@@ -0,0 +1,8 @@
+"""
+@project : Mori
+@author  : zhang_xinjian
+@mail   : zxjlm233@163.com
+@ide    : PyCharm
+@time   : 2020-11-28 09:48:53
+@description: TODO
+"""
