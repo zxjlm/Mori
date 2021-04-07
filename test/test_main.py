d4dc77d9c12d1b21205b58f0dbd5712377c59648
zxjlm <zxjlm233@163.com>
2021-04-07 10:51:33 +0800

    :white_check_mark: add test
    Signed-off-by: SwordHeart <zxjlm233@163.com>

--- test/test_main.py
+++ test/test_main.py
@@ -8,10 +8,25 @@
 """
 import json
 
+import pytest
 import requests
+from rich.console import Console
+from rich.progress import Progress
 
 from antispider.mori_antispider import Antispider
 from decrypt.mori_decrypt import Decrypt
+from mori import mori, processor
+from printer import ResultPrinter
+
+
+@pytest.fixture
+def init_console():
+    return Console()
+
+
+@pytest.fixture
+def init_result_printer(init_console):
+    return ResultPrinter(False, False, init_console)
 
 
 def test_anti_spider():
@@ -37,11 +52,70 @@ def test_encryption():
     url = "http://mori.harumonia.moe/base64/"
     response = requests.get(url)
     assert (
-        response.text != '{"class": [{"name": "A", "total": 64, '
-        '"mid-term-examination": 2, "students": '
-        '[{"number": "1", "name": "Ren", "sex": "F"}, '
-        '{"number": "2", "name": "Tio", "sex": "F"}]}]}'
+            response.text != '{"class": [{"name": "A", "total": 64, '
+                             '"mid-term-examination": 2, "students": '
+                             '[{"number": "1", "name": "Ren", "sex": "F"}, '
+                             '{"number": "2", "name": "Tio", "sex": "F"}]}]}'
     )
     dec_res = Decrypt().decrypt(response.text)
     json_res = json.loads(dec_res)
     assert json_res["class"][0]["name"] == "A"
+
+
+def test_single_mori(init_result_printer, init_console):
+    site_data = {
+        "name": "Mori get api without params",
+        "url": "http://mori.harumonia.moe/",
+        "regex": ["Hello"]
+    }
+    with Progress(console=init_console, auto_refresh=False) as progress:
+        task_id = progress.add_task("Processing ...", total=1)
+        result = processor(
+            site_data,
+            35,
+            False,
+            init_result_printer,
+            task_id,
+            progress,
+        )
+    assert result['status_code'] == 200
+    assert result['check_result'] == 'OK'
+    assert result['check_results']['Hello'] == 'OK'
+
+
+def test_multi_mori(init_console):
+    apis = [
+        {
+            "name": "Mori get api without params | error case | key-error",
+            "url": "http://mori.harumonia.moe/",
+            "regex": ["hello->error"]
+        },
+        {
+            "name": "Mori get api without params | error case | type-error",
+            "url": "http://mori.harumonia.moe/",
+            "regex": ["Hello->error"]
+        },
+        {
+            "name": "Mori get api encrypt",
+            "url": "http://mori.harumonia.moe/base64/",
+            "regex": ["class->$0$->students"],
+            "decrypt": "mori_decrypt"
+        },
+        {
+            "name": "Mori post api antispider",
+            "url": "http://mori.harumonia.moe/token/",
+            "regex": ["result"],
+            "antispider": "mori_antispider",
+            "headers": {
+                "Content-Type": "application/json",
+                "accept": "application/json"
+            },
+            "data": {"token": 0}
+        }
+    ]
+    results = mori(apis, 35, False, False, False)
+    assert results[0]['error_text'] == 'regex failed'
+    assert results[1]['check_result'] == 'Damage'
+    assert results[2]['error_text'] == ''
+    assert results[2]['check_result'] == 'OK'
+    assert results[3]['check_result'] == 'OK'
---
+++ test/test_utils.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    :time: 2021/4/7 10:16
+    :author: Harumonia
+    :url: http://harumonia.moe
+    :project: Mori
+    :file: test_utils.py
+    :copyright: © 2021 harumonia<zxjlm233@gmail.com>
+    :license: MIT, see LICENSE for more details.
+"""
+from utils.data_processor import data_render
+
+
+def test_render_data():
+    apis = [
+        {
+            "name": "Mori get api without params",
+            "url": "http://mori.harumonia.moe/",
+            "regex": ["Hello"],
+            "data": {
+                "t": "{{time}}"
+            }
+        }
+    ]
+    data_render(apis)
+    assert apis[0]['data']['t'] != '{{time}}'
